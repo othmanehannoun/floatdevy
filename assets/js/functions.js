602d44bbfb3a4032219f8bb144b7871eef4a61f9
othmanehannoun <othmanehannoune1@gmail.com>
2025-02-17 12:46:48 +0100

    remove validation phone

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -91,15 +91,10 @@ function submitForm(event) {
     }
 
     // Phone validation
-    // const phoneRegex = /^[0-9]+$/;
     if (!phone) {
         showError("phoneError", "Phone number is required.");
         isValid = false;
-    } 
-    // else if (!phoneRegex.test(phone)) {
-    //     showError("phoneError", "Please enter a valid phone number.");
-    //     isValid = false;
-    // }
+    }
 
     // Message validation
     if (!message) {
